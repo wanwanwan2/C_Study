a897b2af743cbcf75bef81b9c0c57cd00c9a11bb
wanwan2 <121847253+wanwanwan2@users.noreply.github.com>
2023-09-18 16:52:25 +0900

    반환값이 없는 함수

---
+++ 7-3/예제 7-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void print_char(char ch, int count); // 함수 선언
+
+int main(void)
+{
+	print_char('@', 5);
+
+	return 0;
+}
+
+void print_char(char ch, int count)
+{
+	int i;
+
+	for (i = 0; i < count; i++)
+	{
+		printf("%c", ch);
+	}
+
+	return;
+}
